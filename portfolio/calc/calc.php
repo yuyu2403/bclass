6656e8366ed11e623a923b9648f8063f0fe590ad
USENGROUP\a-nozawa <a-nozawa@usen-next.jp>
2023-06-07 16:18:33 +0900

    ごめんなさい

--- portfolio/calc/calc.php
+++ portfolio/calc/calc.php
@@ -24,16 +24,17 @@
         <input type="submit" value="計算">
     </form>
 
+    <footer class="footer">
+        <a href="../index.html" class="footer__return">bclass TOPページへ</a>
+        <ul class="footer__navi flex">
+            <!-- <li><a href="../calc/calc.php">電卓</a></li> -->
+            <li><a href="../form/form.html">フォーム</a></li>
+            <li><a href="../gacha/gacha.php">ガチャ</a></li>
+            <li><a href="../game/game.php">ゲーム</a></li>
+        </ul>
+    </footer>
+
 </body>
-<footer class="footer">
-    <a href="../index.html" class="footer__return">bclass TOPページへ</a>
-    <ul class="footer__navi flex">
-        <!-- <li><a href="../calc/calc.php">電卓</a></li> -->
-        <li><a href="../form/form.html">フォーム</a></li>
-        <li><a href="../gacha/gacha.php">ガチャ</a></li>
-        <li><a href="../game/game.php">ゲーム</a></li>
-    </ul>
-</footer>
 
 </html>
 
--- portfolio/game/game.php
+++ portfolio/game/game.php
@@ -17,17 +17,21 @@
     <form action="game.php">
         <input type="submit" name="gacha" value="戦闘開始">
     </form>
+
+
+    <footer class="footer">
+        <a href="../index.html" class="footer__return">bclass TOPページへ</a>
+        <ul class="footer__navi flex">
+            <li><a href="../calc/calc.php">電卓</a></li>
+            <li><a href="../form/form.html">フォーム</a></li>
+            <li><a href="../gacha/gacha.php">ガチャ</a></li>
+            <!-- <li><a href="../game/game.php">ゲーム</a></li> -->
+        </ul>
+    </footer>
+
+
 </body>
 
-<footer class="footer">
-    <a href="../index.html" class="footer__return">bclass TOPページへ</a>
-    <ul class="footer__navi flex">
-        <li><a href="../calc/calc.php">電卓</a></li>
-        <li><a href="../form/form.html">フォーム</a></li>
-        <li><a href="../gacha/gacha.php">ガチャ</a></li>
-        <!-- <li><a href="../game/game.php">ゲーム</a></li> -->
-    </ul>
-</footer>
 
 
 </html>
