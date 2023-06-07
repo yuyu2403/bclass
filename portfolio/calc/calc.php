59af04b48bfb2ba12da6d0b82f2d4f9ccbb6af47
USENGROUP\ysk-sakai <ysk-sakai@usen-next.jp>
2023-06-07 15:24:22 +0900

    img add

--- portfolio/calc/calc.php
+++ portfolio/calc/calc.php
@@ -1,18 +1,34 @@
-<h1>計算できるで～、数字入れて好きに計算してみて～。。。</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../portfolio/common/style.css">
+    <title>電卓</title>
+</head>
+
+<body>
+    <h1>計算できるで～、数字入れて好きに計算してみて～。。。</h1>
 
 
-<form action="calc.php" method="get">
+    <form action="calc.php" method="get">
 
-    <input type="text" name="num1" id="">
-    </div>
-    <input type="radio" name="enzan" value="a">+
-    <input type="radio" name="enzan" value="b">-
-    <input type="radio" name="enzan" value="c">*
-    <input type="radio" name="enzan" value="d">/
+        <input type="text" name="num1" id="">
+        </div>
+        <input type="radio" name="enzan" value="a">+
+        <input type="radio" name="enzan" value="b">-
+        <input type="radio" name="enzan" value="c">*
+        <input type="radio" name="enzan" value="d">/
 
-    <input type="text" name="num2" id="">
-    <input type="submit" value="計算">
-</form>
+        <input type="text" name="num2" id="">
+        <input type="submit" value="計算">
+    </form>
+</body>
+
+</html>
+
 
 <?php
 if ($_GET) {
